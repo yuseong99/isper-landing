aec29429600b0ef4c9865f894d0ac499fce45260
Yuseong Choi <mondeschoi@Chois-MacBook-Pro.local>
2025-07-12 17:06:53 +0200

    deploy

---
+++ assets/Navigation-63QfzdoJ.js
@@ -0,0 +1,22 @@
+class i{constructor(){this.element=null,this.init()}init(){this.element=document.createElement("nav"),this.element.className="main-nav",this.element.innerHTML=`
+            <div class="nav-container">
+                <a href="/" class="nav-logo">
+                    <img src="/assets/group-107.svg" alt="ISPER" class="logo-img">
+                </a>
+                
+                <div class="nav-links">
+                    <a href="/isper" class="nav-link">Isper</a>
+                    <a href="/maps" class="nav-link">Maps</a>
+                    <a href="/company" class="nav-link">Company</a>
+                    <a href="/news" class="nav-link">News</a>
+                    <a href="/contact" class="nav-link">Contact</a>
+                </div>
+                
+                <div class="nav-mobile-toggle">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        `,this.setupMobileToggle(),this.updateActiveLink()}setupMobileToggle(){const e=this.element.querySelector(".nav-mobile-toggle"),a=this.element.querySelector(".nav-links"),s=this.element.querySelectorAll(".nav-link");e.addEventListener("click",()=>{a.classList.toggle("active"),e.classList.toggle("active")}),s.forEach(t=>{t.addEventListener("click",()=>{a.classList.remove("active"),e.classList.remove("active")})})}updateActiveLink(){const e=window.location.pathname;this.element.querySelectorAll(".nav-link").forEach(s=>{s.getAttribute("href")===e?s.classList.add("active"):s.classList.remove("active")})}render(){return this.element}}export{i as N};
+//# sourceMappingURL=Navigation-63QfzdoJ.js.map
